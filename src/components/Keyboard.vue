c001effcb39a004cbe9be2993446e45d31e1cfb7
Marco <56915570+marcodmrn@users.noreply.github.com>
2020-06-12 15:09:42 +0200

    component for submarine

---
+++ src/components/Keyboard.vue
@@ -0,0 +1,10 @@
+<template>
+  <img src="../assets/keyboard.svg" alt="keys" />
+</template>
+
+<script>
+export default {
+  name: "Keyboard",
+};
+</script>
+<style scoped lang="scss"></style>
--- src/components/Submarine.vue
+++ src/components/Submarine.vue
@@ -1,10 +1,25 @@
 <template>
-  <img src="../assets/shipnolight.svg" alt="ship" />
+  <a href="move" @click="next()">
+    <img src="../assets/shipnolight.svg" alt="ship"
+  /></a>
 </template>
 
 <script>
 export default {
   name: "Submarine",
+  data() {
+    return {
+      actualPage: "move",
+      page: ["move", "find", "search"],
+      count: 0,
+    };
+  },
+  methods: {
+    next: function() {
+      this.actualPage = this.page[this.count];
+      this.count++;
+    },
+  } /* <img :src="'/path/to/images/' + fileName"> */,
 };
 </script>
 <style scoped lang="scss"></style>
