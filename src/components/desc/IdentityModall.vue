f327dfa90d74168ae728dfe28f88cad712445000
Marco <56915570+marcodmrn@users.noreply.github.com>
2020-06-16 15:55:28 +0200

    text identity

--- src/components/desc/IdentityModall.vue
+++ src/components/desc/IdentityModall.vue
@@ -1,5 +1,12 @@
 <template>
-  <img src="../../assets/bubletext.svg" alt="Bubble" />
+  <div class="identity">
+    <p>
+      Hi, my name is Yvette. <br />
+      I am from the pandalidae <br />
+      family and I am a <br />
+      crustacean
+    </p>
+  </div>
 </template>
 
 <script>
@@ -7,4 +14,20 @@ export default {
   name: "IdentityModall",
 };
 </script>
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.identity {
+  background-image: url("../../assets/bubletext.svg");
+  width: 300px;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  p {
+    text-align: center;
+    transform: rotate(-12deg) translateX(70px) translateY(20px);
+    color: #3594ae;
+    font-size: 15px;
+    font-family: "poppins-bold";
+    line-height: 22px;
+  }
+}
+</style>
